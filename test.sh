3e2d461bddcb0a192e8d13c3536d97f02d141b9a
lil-jc <96819541+lil-jc@users.noreply.github.com>
2022-06-27 00:12:49 +0800

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+{ echo "<h1>files</h1><p>"
+for l in $(find); do
+    fname=$(basename $l)
+    [[ $(echo $fname | cut -c1) == '.' ]] && continue
+    [[ $(echo $fname | grep '\.' | wc -l) == 0 ]] && continue
+    echo "$(dirname $l)/<a href='$l'>$fname</a></br>"
+done
+echo "</p>"
+} > index.html
